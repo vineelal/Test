eced64210bb5217d9b96822dfd980deb29e0ed01
Vineela Maddala <vineela.maddala@wavemaker.com>
2017-07-17 14:13:01 +0530

    qwwewewe New files added - [en.json] Files modified - [Common,Login,Main,admin,chart,chart1,charts,chips,fda1,first,footer,form,grid,grid1,grid_chart,header,leftnav,live_list,rightnav,s1,s_grid,test,test1,topnav,hrdb,securityService,data,profiles,.gitignore,.wmproject.properties,pom.xml,project-security.xml] Removed files - [en.json]

--- services/hrdb/src/com/hrdb/dao/EmployeeDao.java
+++ services/hrdb/src/com/hrdb/dao/EmployeeDao.java
@@ -9,7 +9,7 @@ package com.hrdb.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.orm.hibernate4.HibernateTemplate;
+import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.wavemaker.runtime.data.dao.WMGenericDaoImpl;
